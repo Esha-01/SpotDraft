dcbdce4b5d1e350b92369e2291a7b6177759bfa3
Esha <eshakumari593@gmail.com>
2024-02-05 16:30:22 +0530

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import 'bootstrap/dist/css/bootstrap.css'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,7 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) {    
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
